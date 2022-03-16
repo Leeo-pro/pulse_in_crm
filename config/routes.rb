59d7b25d063bce814d42bcbaf046ebfd2b0fa44e
Leeo-pro <mineva.aug@gmail.com>
2022-03-17 08:11:47 +0900

    administrate実装-20220317(リモートからの修正対応)

--- config/routes.rb
+++ config/routes.rb
@@ -8,33 +8,26 @@ Rails.application.routes.draw do
     root to: 'admins#index'
   end
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-
   # admin関連=========================================================
   devise_for :admins, controllers: {
     sessions: 'admins/sessions'
   }
-
   # =================================================================
-
   # user関連==========================================================
   devise_scope :user do
     root 'users/sessions#new'
   end
-
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     confirmations: 'users/confirmations',
     registrations: 'users/registrations'
   }
-
   namespace :users do
     resources :dash_boards, only: [:index]
     resource :profile, except: %i[create new]
   end
-
   # =================================================================
-
   # 共通==============================================================
   # 利用規約
   get 'use' => 'use#index'
