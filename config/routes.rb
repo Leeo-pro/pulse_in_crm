d5e86d89d5d3042c47d0bb43152cccb17ccd6a0d
Leeo-pro <mineva.aug@gmail.com>
2022-03-17 08:24:05 +0900

    administrate実装-20220317_3

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,18 @@ Rails.application.routes.draw do
   # =================================================================
   # user関連==========================================================
   devise_scope :user do
-    root 'users/sessions#new'
+    get ':company_id/signin' => 'users/sessions#new', as: :user_login
+    post ':company_id/signin' => 'users/sessions#create', as: :user_session
+    delete '/users/sign_out' => 'users/sessions#destroy', as: :destroy_user_session
+    get '/cancel' => 'users/registrations#cancel', as: :cancel_user_registration
+    get '/sign_up' => 'users/registrations#new', as: :new_user_registration
+    post '/sign_up' => 'users/registrations#create', as: :create_user_registration
+    get '/edit' => 'users/registrations#edit', as: :edit_user_registration
+    put '/users' => 'users/registrations#update', as: :user_registration
   end
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
+  devise_for :users, skip: %i[registrations sessions], controllers: {
     passwords:     'users/passwords',
-    confirmations: 'users/confirmations',
-    registrations: 'users/registrations'
+    confirmations: 'users/confirmations'
   }
   namespace :users do
     resources :dash_boards, only: [:index]
@@ -29,6 +34,10 @@ Rails.application.routes.draw do
   end
   # =================================================================
   # 共通==============================================================
+  # トップページ
+  root 'use#top'
+  # アカウント登録後ページ
+  get 'registration_comp' => 'use#registration_comp'
   # 利用規約
   get 'use' => 'use#index'
   # =================================================================
