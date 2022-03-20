409e94a11096f57afa0b276803fa521cfc2faf3e
yusamasaki <masa.aw1208@gmail.com>
2022-03-20 22:20:00 +0900

    application_mailer修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: '遊佐 <from@example.com>'
+  default from: 'システム <from@example.com>'
   layout 'mailer'
 end
