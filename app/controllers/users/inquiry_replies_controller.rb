5f54b306d8f6eadbb16e08a41c2d0e53bb12b538
Ryousuke-Tanaka55 <tianzhongliangyou312@gmail.com>
2022-03-21 14:12:20 +0900

    メール画面表示_2022032101

--- app/controllers/users/inquiry_replies_controller.rb
+++ app/controllers/users/inquiry_replies_controller.rb
@@ -1,17 +1,17 @@
 module Users
-  class InqueryRepliesController < Users::Base
+  class InquiryRepliesController < Users::Base
 
     def show
     end
 
     def new
-      @inquiry_reply = Inquiry_reply.new
+      @inquiry_reply = InquiryReply.new
     end
 
     def create
-      @inquiry_reply = Inquiry_reply.new(inquiry_reply_params)
+      @inquiry_reply = InquiryReply.new(inquiry_reply_params)
       if @inquiry_reply.save
-        Inquiry_replyMailer.inquiry_reply(@inquiry_reply).deliver  #メール送信処理追加
+        InquiryReplyMailer.inquiry_reply(@inquiry_reply).deliver  #メール送信処理追加
         redirect_to users_inquiry_reply_url, notice: '返信メールを送信しました'
       else
         render :new
--- spec/mailers/previews/inquiry_reply_preview.rb
+++ spec/mailers/previews/inquiry_reply_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/inquiry_reply
 class InquiryReplyPreview < ActionMailer::Preview
+  def inquiry_reply
+    inquiry_reply = InqueryReply.new(title: "テスト", content: "問い合わせメッセージ")
 
+    InquiryReplyMailer.inquiry_reply(inquiry_reply)
+  end
 end
