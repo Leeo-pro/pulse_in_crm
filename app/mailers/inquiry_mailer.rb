c7b2def63c4747452bbecdd7cd34625182439125
yusamasaki <masa.aw1208@gmail.com>
2022-03-23 12:26:43 +0900

    uuid適応

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -3,8 +3,10 @@ class InquiryMailer < ApplicationMailer
     @user = current_user
     @answer = inquiry_reply.content
 
-    inquiry_reply.files.each do |file|
-      attachments[file.filename] = File.read("public/uploads/#{file.model.class.to_s.underscore}/#{file.mounted_as}/#{file.model.id}/#{file.filename}")
+    if inquiry_reply.files.present?
+      inquiry_reply.files.each do |file|
+        attachments[file.filename] = File.read("public/uploads/#{file.model.class.to_s.underscore}/#{file.mounted_as}/#{file.model.id}/#{file.filename}")
+      end
     end
 
     mail to: @user.email, subject: '【サイト名】 お問い合わせありがとうございます'
