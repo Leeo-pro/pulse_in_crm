d5e7128581719db34a29fddd77d1d7cbb7faf842
yusamasaki <masa.aw1208@gmail.com>
2022-03-25 13:11:42 +0900

    inquiry_replies_controller記述変更。module Users→Users::InquiryRepliesControllerに変更

--- app/controllers/users/inquiry_replies_controller.rb
+++ app/controllers/users/inquiry_replies_controller.rb
@@ -1,32 +1,30 @@
-module Users
-  class InquiryRepliesController < Users::Base
-    def new
-      # テスト用にフォームに文字を入れておきました
-      @inquiry_reply = InquiryReply.new(
-        title:   'XXXXXXX',
-        content: 'XXXXXXX'
-      )
-    end
+class Users::InquiryRepliesController < Users::Base
+  def new
+    # テスト用にフォームに文字を入れておきました
+    @inquiry_reply = InquiryReply.new(
+      title:   'XXXXXXX',
+      content: 'XXXXXXX'
+    )
+  end
 
-    def create
-      @inquiry_reply = InquiryReply.new(inquiry_reply_params)
-      if @inquiry_reply.save
-        # メールを送信する機能。今回は仮にメソッドの変数にcurrent_user、@inquiry_replyを付与
-        InquiryMailer.send_when_company_reply(current_user, @inquiry_reply).deliver
-        redirect_to users_inquiry_reply_url(@inquiry_reply)
-      else
-        render 'new'
-      end
+  def create
+    @inquiry_reply = InquiryReply.new(inquiry_reply_params)
+    if @inquiry_reply.save
+      # メールを送信する機能。今回は仮にメソッドの変数にcurrent_user、@inquiry_replyを付与
+      InquiryMailer.send_when_company_reply(current_user, @inquiry_reply).deliver
+      redirect_to users_inquiry_reply_url(@inquiry_reply)
+    else
+      render 'new'
     end
+  end
 
-    def show
-      @inquiry_reply = InquiryReply.find(params[:id])
-    end
+  def show
+    @inquiry_reply = InquiryReply.find(params[:id])
+  end
 
-    private
+  private
 
-    def inquiry_reply_params
-      params.require(:inquiry_reply).permit(:title, :content, { files: [] }).merge(to_email: current_user.email) # 仮にcurrent_userのemailを保存
-    end
+  def inquiry_reply_params
+    params.require(:inquiry_reply).permit(:title, :content, { files: [] }).merge(to_email: current_user.email) # 仮にcurrent_userのemailを保存
   end
 end
