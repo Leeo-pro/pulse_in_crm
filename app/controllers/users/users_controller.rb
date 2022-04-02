e124374c285c3c9dec06436139b1faa13c6110c2
yusamasaki <masa.aw1208@gmail.com>
2022-04-02 11:32:03 +0900

    ユーザー一覧・詳細・編集画面作成/モデル日本語化

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,8 +1,14 @@
 class Users::UsersController < Users::Base
-  def index; end
+  before_action :set_user, except: %i[index new create]
+  before_action :user_access_authorization, except: %i[index new create]
+  before_action :current_company
+
+  def index
+    @users = @current_company.users.where(role: 0)
+  end
 
   def new
-    @user = User.new(
+    @user = @current_company.users.new(
       name:  'test',
       email: 'test@email.com'
     )
@@ -13,7 +19,7 @@ class Users::UsersController < Users::Base
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "#{@user.name}の作成に成功しました"
-      redirect_to users_user_path(@user)
+      redirect_to users_user_url(@user)
     else
       render :new
     end
@@ -23,9 +29,20 @@ class Users::UsersController < Users::Base
 
   def edit; end
 
-  def update; end
+  def update
+    if @user.update(user_params)
+      flash[:success] = '更新しました'
+      redirect_to users_user_url(@user)
+    else
+      render 'edit'
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @user.destroy!
+    flash[:danger] = "#{@user.name}のユーザー情報を削除しました"
+    redirect_to users_users_url
+  end
 
   private
 
@@ -34,6 +51,18 @@ class Users::UsersController < Users::Base
       access_authorization_attributes: %i[
         id inquiry_browse inqury_reply inqury_form_setting
       ]
-    ).merge(company_id: current_user.company.id, role: 0)
+    ).merge(role: 0)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_access_authorization
+    @access_authorization = @user.access_authorization
+  end
+
+  def current_company
+    @current_company ||= current_user&.company
   end
 end
