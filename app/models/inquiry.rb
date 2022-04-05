3f4e1bf14595afc49cbfa727f4c5acf8cc72ae8f
RumiNakamura <krw.psh2196@gmail.com>
2022-04-05 16:06:12 +0900

    関連付け設定追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,7 +3,7 @@ class Inquiry < ApplicationRecord
 
   # 親テーブル(inquiry_form)作成後に関連付け設定
   # belongs_to :inquiry_form
-  has_one :inquiry_input_content
+  has_many :inquiry_input_contents
 
   enum status: { 未読: 0, 既読未対応: 1, 既読対応中: 2, 対応済: 3 }, _prefix: :status
 end
