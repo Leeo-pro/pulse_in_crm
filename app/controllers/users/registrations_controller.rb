09f89f1d60a51b9a70510c6c7f6a27be7f0cc167
ikd-gifu <oki19ki@gmail.com>
2022-04-06 09:06:23 +0900

    Fix registrations_controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,7 @@ module Users
     def new
       @user = User.new
       @user.build_company
-      @company.build_privacy_policy
+      @user.company.build_privacy_policy
       @hinagata = "<h1>プライバシーポリシー</h1>"
       email_arr = User.pluck(:email)
       gon.email_arr = email_arr
