b2dbeef2a8ac592c5b0a4f6f5dbc122de2d4b506
RumiNakamura <krw.psh2196@gmail.com>
2022-04-10 08:55:30 +0900

    Inquiryテーブルのcompany_idカラムにindex追加

--- db/migrate/20220409123621_add_company_id_to_inquiries.rb
+++ db/migrate/20220409123621_add_company_id_to_inquiries.rb
@@ -1,5 +1,6 @@
 class AddCompanyIdToInquiries < ActiveRecord::Migration[6.1]
   def change
     add_column :inquiries, :company_id, :string, null: false
+    add_index :inquiries, :company_id
   end
 end
