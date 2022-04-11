20f3025c822ce3f1ab9476ba0ae260e3f6e1689a
Ryousuke-Tanaka55 <tianzhongliangyou312@gmail.com>
2022-04-11 23:33:30 +0900

    アクセス制限メソッド作成

--- app/models/access_authorization.rb
+++ app/models/access_authorization.rb
@@ -1,3 +1,19 @@
 class AccessAuthorization < ApplicationRecord
   belongs_to :user
+
+  # お問い合わせ内容の閲覧制限
+  def inquiry_browse_access
+    redirect_to users_dash_boards_path, flash: { danger: 'アクセス権限がありません' } unless current_user.access_authorization.inquiry_browse?
+  end
+
+  # お問い合わせ内容返信の制限
+  def inquiry_reply_access
+    redirect_to users_dash_boards_path, flash: { danger: 'アクセス権限がありません' } unless current_user.access_authorization.inquiry_reply?
+  end
+
+  # お問い合わせフォーム項目設定の制限
+  def iinquiry_form_setting_access
+    redirect_to users_dash_boards_path, flash: { danger: 'アクセス権限がありません' } unless current_user.access_authorization.inquiry_form_setting?
+  end
+
 end
