b6525bfd96c3f6ecffb0a3e7e8964f2cae92f78f
ikd-gifu <oki19ki@gmail.com>
2022-04-14 14:51:07 +0900

    Fix PR-controller

--- app/controllers/users/privacy_policy_controller.rb
+++ app/controllers/users/privacy_policy_controller.rb
@@ -8,7 +8,7 @@ class Users::PrivacyPolicyController < Users::Base
     @privacy_policy = PrivacyPolicy.find(params[:id])
     if @privacy_policy.update(privacy_policy_params)
       flash[:success] = "プライバシーポリシーを更新しました"
-      # /users/iquiry_form/:id/edit へ遷移
+      # TODO:  /users/iquiry_form/:id/edit へ遷移
     else
       render 'edit'
     end
