5179cca54baf631a0da48c4ea2e8e895d284f675
ikd-gifu <oki19ki@gmail.com>
2022-04-15 17:47:19 +0900

    Modify-schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_132427) do
+ActiveRecord::Schema.define(version: 2022_04_09_123621) do
+
+  create_table "access_authorizations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.boolean "inquiry_browse"
+    t.boolean "inqury_reply"
+    t.boolean "inqury_form_setting"
+    t.string "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -51,6 +60,27 @@ ActiveRecord::Schema.define(version: 2022_04_02_132427) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["company_id"], name: "index_privacy_policies_on_company_id"
+  create_table "inquiries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "company_id", null: false
+    t.index ["company_id"], name: "index_inquiries_on_company_id"
+  end
+
+  create_table "inquiry_input_contents", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "inquiry_replies", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "to_email"
+    t.json "files"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -75,7 +105,6 @@ ActiveRecord::Schema.define(version: 2022_04_02_132427) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "age"
-    t.integer "gender"
     t.string "company_id"
     t.integer "role", default: 1
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
