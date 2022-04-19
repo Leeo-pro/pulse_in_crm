6d2318a7529573bf3ae17cbea27766cf9297a823
Ryousuke-Tanaka55 <tianzhongliangyou312@gmail.com>
2022-04-19 23:21:20 +0900

    if文条件式改良

--- app/controllers/users/base.rb
+++ app/controllers/users/base.rb
@@ -24,21 +24,21 @@ module Users
 
     # お問い合わせ内容の閲覧制限
     def inquiry_browse_access
-      if current_user.role_other? || current_user.access_authorization.inquiry_browse == false
+      if current_user.role_other? || !current_user.access_authorization.inquiry_browse?
         redirect_to users_dash_boards_path, flash: { danger: 'アクセス権限がありません' }
       end
     end
 
     # お問い合わせ内容返信の制限
     def inquiry_reply_access
-      if current_user.role_other? || current_user.access_authorization.inquiry_reply == false
+      if current_user.role_other? || !current_user.access_authorization.inquiry_reply?
         redirect_to users_dash_boards_path, flash: { danger: 'アクセス権限がありません' }
       end
     end
 
     # お問い合わせフォーム項目設定の制限
     def inquiry_form_setting_access
-      if current_user.role_other? || current_user.access_authorization.inquiry_form_setting == false
+      if current_user.role_other? || !current_user.access_authorization.inquiry_form_setting?
         redirect_to users_dash_boards_path, flash: { danger: 'アクセス権限がありません' }
       end
     end
