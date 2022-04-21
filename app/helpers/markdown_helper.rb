6efd7a5f6b1dcaff8ae770bd924f2a47ce0020ed
ikd-gifu <oki19ki@gmail.com>
2022-04-21 22:14:37 +0900

    Fix test-offences

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,27 +1,27 @@
 # gem 'redcarpet' 用
 module MarkdownHelper
-require 'redcarpet'
+  require 'redcarpet'
 
   def markdown(text)
     options = {
-      filter_html:         true, # htmlを出力しない
-      hard_wrap:           true, # マークダウン中の空行をhtmlに置き換え
-      space_after_headers: true  # # と文字の間にスペースを要求
-    }
+                filter_html:         true, # htmlを出力しない
+                hard_wrap:           true, # マークダウン中の空行をhtmlに置き換え
+                space_after_headers: true  # # と文字の間にスペースを要求
+              }
 
     extensions = {
-      autolink:           true,  # <>で囲まれてなくてもリンクを認識
-      no_intra_emphasis:  true,  # 単語中の強調を認識しない
-      fenced_code_blocks: true,  # フェンスのあるコードブロックを認識
-      strikethrough:      true,  # ~ 2つで取り消し線
-      superscript:        true,  # ^ の後ろが上付き文字
-      tables:             true,  # テーブルを認識
-      underline:          true,  # 斜線(* *)
-      highlight:          true,  # ハイライト(== ==)
-      quote:              true,  # 引用符(" ")
-      footnotes:          true,  # 脚注( ^[1] )
-      lax_html_blocks:    true   # HTMLブロックの上下の空行を不要にする
-    }
+                  autolink:           true,  # <>で囲まれてなくてもリンクを認識
+                  no_intra_emphasis:  true,  # 単語中の強調を認識しない
+                  fenced_code_blocks: true,  # フェンスのあるコードブロックを認識
+                  strikethrough:      true,  # ~ 2つで取り消し線
+                  superscript:        true,  # ^ の後ろが上付き文字
+                  tables:             true,  # テーブルを認識
+                  underline:          true,  # 斜線(* *)
+                  highlight:          true,  # ハイライト(== ==)
+                  quote:              true,  # 引用符(" ")
+                  footnotes:          true,  # 脚注( ^[1] )
+                  lax_html_blocks:    true   # HTMLブロックの上下の空行を不要にする
+                }
 
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
--- spec/requests/users/privacy_policy_spec.rb
+++ spec/requests/users/privacy_policy_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Users::PrivacyPolicies', type: :request do
 
-  before(:each) do
-    @company = Company.create(name: 'テスト会社')
-    @privacy_policy = PrivacyPolicy.create(content: 'テスト雛形', company_id: @company.id)
-  end
+  # before(:each) do
+  #   @company = Company.create(name: 'テスト会社')
+  #   @privacy_policy = PrivacyPolicy.create(content: 'テスト雛形', company_id: @company.id)
+  # end
 
   describe 'POST /create' do
+    let!(:company) { Company.create(name: 'テスト') }
+    let!(:privacy_policy) { PrivacyPolicy.create(content: 'テスト雛形', company_id: company.id) }
+
     it 'returns http success' do
       get '/sign_up'
       expect(response).to have_http_status(:success)
@@ -15,15 +18,21 @@ RSpec.describe 'Users::PrivacyPolicies', type: :request do
   end
 
   describe 'GET /edit' do
+    let!(:company) { Company.create(name: 'テスト') }
+    let!(:privacy_policy) { PrivacyPolicy.create(content: 'テスト雛形', company_id: company.id) }
+
     it 'returns http success' do
-      get edit_users_privacy_policy_path(@privacy_policy)
+      get edit_users_privacy_policy_path(privacy_policy)
       expect(response).to have_http_status(:redirect)
     end
   end
 
   describe 'PATCH /update' do
+    let!(:company) { Company.create(name: 'テスト') }
+    let!(:privacy_policy) { PrivacyPolicy.create(content: 'テスト雛形', company_id: company.id) }
+
     it 'returns http success' do
-      patch users_privacy_policy_path(@privacy_policy)
+      patch users_privacy_policy_path(privacy_policy)
       expect(response).to have_http_status(:redirect)
     end
   end
