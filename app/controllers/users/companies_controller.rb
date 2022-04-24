cdb1fb8931d3f05c90341e830f9be7e8127cd690
RumiNakamura <krw.psh2196@gmail.com>
2022-04-24 16:45:21 +0900

    企業メールアドレス編集機能を実装、show、editのビューの見た目を調整

--- app/controllers/users/companies_controller.rb
+++ app/controllers/users/companies_controller.rb
@@ -2,10 +2,29 @@
 
 module Users
   class CompaniesController < Users::Base
-    def show
-      @current_company = current_company
-    end
+  before_action :set_company
+
+    def show; end
+    
     def edit; end
-    def update; end
+
+    def update
+      if @company.update(company_email_params)
+        flash[:success] = '企業メールアドレスを更新しました'
+        redirect_to users_company_url
+      else
+        render 'edit'
+      end
+    end
+
+    private
+
+    def set_company
+      @company = current_company
+    end
+
+    def company_email_params
+      params.require(:company).permit(:email)
+    end
   end
 end
