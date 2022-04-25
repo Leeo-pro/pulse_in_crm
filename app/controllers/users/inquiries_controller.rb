143334abe49f2cd1729a8f27d2621bff5fbf1c0f
RumiNakamura <krw.psh2196@gmail.com>
2022-04-25 16:58:07 +0900

    Inquiriesテーブルのidをuuid対応に修正、createアクション追記

--- app/controllers/users/inquiries_controller.rb
+++ app/controllers/users/inquiries_controller.rb
@@ -1,7 +1,11 @@
 module Users
   class InquiriesController < Users::Base
-    def create 
-      flash[:info] = "お問い合せ作成テスト"
+    def create
+      if current_company.inquiries.create
+        flash[:success] = "お問い合わせデータが作成されました"
+      else
+        flash[:danger] = "お問い合わせデータの作成に失敗しました"
+      end
       redirect_to users_dash_boards_path
     end
   end
