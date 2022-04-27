2aeaaa02cb061f86eb7a60cd60619707f5d50d45
RumiNakamura <krw.psh2196@gmail.com>
2022-04-27 12:23:53 +0900

    コントローラのクラス名記述を修正

--- app/controllers/users/companies_controller.rb
+++ app/controllers/users/companies_controller.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-module Users
-  class CompaniesController < Users::Base
-    before_action :set_company
+class Users::CompaniesController < Users::Base
+  before_action :set_company
 
-    def show; end
+  def show; end
 
-    def edit; end
+  def edit; end
 
-    def update
-      if @company.update(company_email_params)
-        flash[:success] = '企業メールアドレスを更新しました'
-        redirect_to users_company_url
-      else
-        render 'edit'
-      end
+  def update
+    if @company.update(company_email_params)
+      flash[:success] = '企業メールアドレスを更新しました'
+      redirect_to users_company_url
+    else
+      render 'edit'
     end
+  end
 
-    private
+  private
 
-    def set_company
-      @company = current_company
-    end
+  def set_company
+    @company = current_company
+  end
 
-    def company_email_params
-      params.require(:company).permit(:email)
-    end
+  def company_email_params
+    params.require(:company).permit(:email)
   end
 end
