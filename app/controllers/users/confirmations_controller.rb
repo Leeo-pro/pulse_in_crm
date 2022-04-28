8fef3dfa8fad88bef1dd443a61e076bfecbecf75
ikd-gifu <oki19ki@gmail.com>
2022-04-28 22:11:46 +0900

    Fix confirmation-redirect-routing

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -19,9 +19,22 @@ module Users
     end
 
     # GET /resource/confirmation?confirmation_token=abcdef
-    # def show
-    #   super
-    # end
+    def show
+      @users = User.where.not(confirmed_at: nil)
+      email_arr = @users.pluck(:email)
+      gon.email_arr = email_arr
+
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
+
+      if resource.errors.empty?
+        set_flash_message!(:notice, :confirmed)
+        respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      else
+        set_flash_message!(:notice, :already_confirmed)
+        respond_with_navigational(resource.errors, status: :unprocessable_entity){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      end
+    end
 
     # protected
 
