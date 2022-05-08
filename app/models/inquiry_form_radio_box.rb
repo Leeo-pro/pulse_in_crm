7406c961fab3f009a5d96e3178cac4170736d124
HiroakiKonno <k.hiroaki0708@gmail.com>
2022-05-08 14:18:34 +0900

    bin/test修正

--- app/models/inquiry_form_radio_box.rb
+++ app/models/inquiry_form_radio_box.rb
@@ -2,5 +2,5 @@ class InquiryFormRadioBox < ApplicationRecord
   before_create :set_uuid
   belongs_to :inquiry_form
   has_many :inquiry_form_radio_box_contents, dependent: :destroy
-  enum content: { select_item1: 1, select_item2: 2, select_item3: 3, select_item4: 4, select_item4: 5 }
+  enum content: { select_item1: 1, select_item2: 2, select_item3: 3, select_item4: 4, select_item5: 5 }
 end
